6eefd8a6e014e64dc9ed4fce6ddc45f6891b28c8
Steven <stevensvg59@gmail.com>
2023-11-04 23:15:51 -0500

    Learning Activity - C# Prep 2 + Stretch challenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string grade = "";
+        string sign = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+        else if (percentage < 60)
+        {
+            grade = "F";
+        }
+
+        //Stretch Challenges
+        if (grade != "F" && percentage % 10 < 3)
+        {
+            sign = "-";
+        }
+        else if (grade != "A" && grade != "F" && percentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+
+
+        Console.WriteLine($"Your grade is {grade}{sign}.");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else {
+            Console.WriteLine("Oh no, you didn't pass the course. Better luck next time.");
+        }
     }
 }
