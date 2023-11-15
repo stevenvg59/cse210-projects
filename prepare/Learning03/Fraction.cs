b2ab63b3eab42f4f11e73dcb450dd950ebab2e0e
Steven <stevensvg59@gmail.com>
2023-11-15 17:48:37 -0500

    W3 Learning Activities until Step 5 (it's not completed yet)

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction() {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top) {
+        _top = top;
+        _bottom = 1;
+    }
+
+
+    public Fraction(int top, int bottom) {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void Display() {
+        Console.WriteLine("Top: " + _top +" and bottom: " + _bottom );
+    }
+
+    public int GetTop() {
+        return _top;
+    }
+
+    public void SetTop(int top) {
+        _top = top;
+    }
+
+    public int GetBottom() {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom) {
+        _bottom = bottom;
+    }
+}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.Display();
+        fraction2.Display();
+        fraction3.Display();
+
+        fraction1.SetTop(8);
+        fraction2.SetBottom(9);
+        Console.WriteLine(fraction1.GetTop());
+        Console.WriteLine(fraction2.GetBottom());
     }
 }
