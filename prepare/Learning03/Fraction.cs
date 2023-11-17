d1bbcd2f4dc9a322fb534b99bcffbf02b65f8408
Steven <stevensvg59@gmail.com>
2023-11-17 10:19:24 -0500

    W03 Learning activity - Fraction

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,39 +5,62 @@ public class Fraction
     private int _top;
     private int _bottom;
 
-    public Fraction() {
+    public Fraction() //Constructor with no parameteres
+    { 
         _top = 1;
         _bottom = 1;
     }
 
-    public Fraction(int top) {
+    public Fraction(int top) //Constructor with only numerator (top)
+    {
         _top = top;
         _bottom = 1;
     }
 
 
-    public Fraction(int top, int bottom) {
+    public Fraction(int top, int bottom) //Constructor with both numerator and denominator
+    {
         _top = top;
         _bottom = bottom;
     }
 
-    public void Display() {
+    public void Display() //Testing the display for each part of the fraction
+    {
         Console.WriteLine("Top: " + _top +" and bottom: " + _bottom );
     }
 
-    public int GetTop() {
+    //GETTERS
+    public int GetTop() 
+    {
         return _top;
     }
 
-    public void SetTop(int top) {
-        _top = top;
-    }
-
-    public int GetBottom() {
+    public int GetBottom() 
+    {
         return _bottom;
     }
 
-    public void SetBottom(int bottom) {
+    //SETTERS
+    public void SetTop(int top) 
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom) 
+    {
         _bottom = bottom;
+    }
+
+    public string GetFractionString() //Getting the form "3/4" fraction string
+    {
+
+        return ($"{_top}/{_bottom}");
+    }
+
+    public double GetDecimalValue() //Computing the decimal value using double (decimals)
+    {
+        double result;
+        result = (double)_top/(double)_bottom;
+        return result;
     }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,17 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
+        //Creating new instances of Fraction for different cases
         Fraction fraction1 = new Fraction();
-        Fraction fraction2 = new Fraction(6);
-        Fraction fraction3 = new Fraction(6, 7);
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction(1, 3);
 
+        //Testing display
         fraction1.Display();
         fraction2.Display();
         fraction3.Display();
+        fraction4.Display();
 
-        fraction1.SetTop(8);
-        fraction2.SetBottom(9);
-        Console.WriteLine(fraction1.GetTop());
-        Console.WriteLine(fraction2.GetBottom());
+        //Getting results calling the appropriate functions for each instance
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
