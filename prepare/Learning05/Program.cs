7032d9dcd62034c76292bc5a4813976513ce630d
Steven <stevensvg59@gmail.com>
2023-12-06 01:43:20 -0500

    Learning Activities 04 - just create classes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5);
+        Console.WriteLine("The color is: " + square.GetColor());
+        Console.WriteLine("The area is: " + square.GetArea());
     }
 }
