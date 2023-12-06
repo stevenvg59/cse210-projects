41897f25a830acab171d9d21c01ac28216d9844e
Steven <stevensvg59@gmail.com>
2023-12-06 14:32:29 -0500

    Learning Activities 04 - completed

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,5 +7,13 @@ class Program
         Square square = new Square("Red", 5);
         Console.WriteLine("The color is: " + square.GetColor());
         Console.WriteLine("The area is: " + square.GetArea());
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 4);
+        Console.WriteLine("The color is: " + rectangle.GetColor());
+        Console.WriteLine("The area is: " + rectangle.GetArea());
+
+        Circle circle = new Circle("Yellow", 10);
+        Console.WriteLine("The color is: " + circle.GetColor());
+        Console.WriteLine("The area is: " + Math.Round(circle.GetArea(), 2));
     }
 }
