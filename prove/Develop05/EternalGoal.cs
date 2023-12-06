f3d1d63da4a8d150382040822e4cb866e0feedeb
Steven <stevensvg59@gmail.com>
2023-12-06 16:03:04 -0500

    Dev 05 Program - Eternal Quest, only basic methods and structure

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,25 @@
+public class EternalGoal : Goal
+{
+
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public override void RecordEvent()
+    {
+        
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,24 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
